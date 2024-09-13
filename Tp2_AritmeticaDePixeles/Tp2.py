ebc71a1bedc32145a4e3bf21bfc7909914b15f75
Miguel Diaz <migueldi.tech@gmail.com>
2024-09-13 14:51:48 -0300

    Cambio de nombre de carpetas para facilitar la navegacion

--- Tp2_AritmeticaDePixeles/Tp2.py
+++ Tp2_AritmeticaDePixeles/Tp2.py
@@ -33,14 +33,13 @@ imagen_inicio = None # imagen que mostrara al principio
 imagen_final = None # imagen que mostrara al final
 
 #FUNCIONES PARA TRABAJAR CON ELLAS (Deberia modularizarse)
-def rgb2yiq(imagen):
+def rgb2yiq(imagen, luminancia =1, saturacion=1):
     imagen = np.clip(imagen/255.,0.,1.) # imagen = imagen/255.
     #print("rgb2yiq: ",imagen.shape,imagen.dtype, "tamaño: ", imagen.min(),imagen.max())
     yiq= np.zeros(imagen.shape)
     yiq[:,:,0] = np.clip(0.299 * imagen[:,:,0] + 0.587 * imagen[:,:,1] + 0.114 * imagen[:,:,2], 0., 1.)
     yiq[:,:,1] = np.clip(0.595716 * imagen[:,:,0] - 0.274453 * imagen[:,:,1] - 0.321263 * imagen[:,:,2], -0.5957, 0.5957)
     yiq[:,:,2] = np.clip(0.211456 * imagen[:,:,0] - 0.522591 * imagen[:,:,1] + 0.311135 * imagen[:,:,2], -0.5226, 0.5226)
-    luminancia, saturacion = preguntar_luminosidad()
     yiq[:,:,0] *=luminancia
     yiq[:,:,1] *=saturacion
     yiq[:,:,2] *=saturacion
@@ -55,10 +54,10 @@ def yiq2rgb(imagen):
     rgb[:,:,2] = np.clip(imagen[:,:,0] - 1.1070 * imagen[:,:,1] + 1.7046 * imagen[:,:,2], 0., 1.)
     return rgb
 
-def preguntar_luminosidad():
-    luminancia = simpledialog.askfloat("Entrada", "Ingrese el valor de luminancia:")
-    saturacion = simpledialog.askfloat("Entrada", "Ingrese el valor de saturacion:")
-    return luminancia, saturacion
+def cuasi_suma_rgb(imagen_inicio, imagen_final):
+    im = np.clip(imagen_inicio + imagen_final, 0, 255).astype(np.uint8)
+    plt.imshow(im)
+    plt.show()
 
 # FUNCIONES DE INTERFAZ (Tambien deberia modularizarse)
 def abrir_imagen():
@@ -77,7 +76,9 @@ def transformar_a_yiq():
     global imagen_inicio, imagen_final
     if imagen_inicio is not None:
         # Transformar la imagen
-        imagen_final = rgb2yiq(imagen_inicio)
+        luminancia = simpledialog.askfloat("Entrada", "Ingrese el valor de luminancia:") 
+        saturacion = simpledialog.askfloat("Entrada", "Ingrese el valor de saturacion:")
+        imagen_final = rgb2yiq(imagen_inicio,luminancia, saturacion)
         imagen_final = (imagen_final * 255).astype(np.uint8)
         # Mostrar la imagen en el canvas derecho utilizando Matplotlib
         ax2.clear()  # Limpiar el contenido anterior de la figura
@@ -96,24 +97,38 @@ def transformar_a_rgb():
         canvas2.draw()  # Dibujar la imagen en el canvas de Matplotlib
 
 
-def luminancia():
+def oscurecer_imagen():
     global imagen_inicio, imagen_final
     if imagen_inicio is not None:
-        luminancia,saturacion = preguntar_luminosidad()
+        valorOscurecer= simpledialog.askfloat("Entrada", "Ingrese valor para Oscurecer la imagen:") 
         if imagen_final is not None:
-            imagen_final = np.clip(imagen_final /255.,0.,1.) #normalizo los valores
+            imagen_final = imagen_final / 255.0 #normalizo los valores
         else:
-            imagen_final = np.clip(imagen_inicio /255.,0.,1.) #normalizo los valores
-        imagen_final[:,:,0] *=luminancia
-        imagen_final[:,:,1] *=saturacion
-        imagen_final[:,:,2] *=saturacion
-        # Convertimos luego de normalizar a un rango de 0 a 255 que maneja pil
+            imagen_final = imagen_inicio / 255.0 #normalizo los valores
+        imagen_final= imagen_final * valorOscurecer
         imagen_final = (imagen_final * 255).astype(np.uint8)
         ax2.clear()  # Limpiar el contenido anterior de la figura
         ax2.imshow(imagen_final)  # Mostrar la imagen
         ax2.axis('off')  # Ocultar los ejes
         canvas2.draw()  # Dibujar la imagen en el canvas de Matplotlib
+
+def suma_clampeada():
+    global imagen_inicio, imagen_final
+    ruta = filedialog.askopenfilename(defaultextension=".png", filetypes=[("Image files", "*.png ; *.bmp ; *.jpg")])
+    if ruta:
+        imagen_final = imageio.v2.imread(ruta)  #Reemplazar por lectura de imageio
+        # Mostrar la imagen en el canvas derecho utilizando Matplotlib
+        ax2.clear()  # Limpiar el contenido anterior de la figura
+        ax2.imshow(imagen_final)  # Mostrar la imagen
+        ax2.axis('off')  # Ocultar los ejes
+        canvas2.draw()  # Dibujar la imagen en el canvas de Matplotlib
+        #sumita
+        cuasi_suma_rgb(imagen_inicio, imagen_final)
+
         
+def ejecutar_opcion():
+    funcion_seleccionada = operaciones[opcion.get()]
+    funcion_seleccionada()
 
 def volver():
     global imagen_inicio, imagen_final
@@ -132,13 +147,24 @@ def guardar_imagen():
         if ruta_guardado:
             imageio.imwrite(ruta_guardado, imagen_final)
             
+# Diccionario de funciones
+operaciones = {
+    "   Transformar a YIQ": transformar_a_yiq,
+    "   Transformar a RGB": transformar_a_rgb,
+    "   Oscurecer imagen ": oscurecer_imagen,
+    "   Sumar una imagen ": suma_clampeada
+}
+# Variable para almacenar la opción seleccionada
+opcion = tk.StringVar()
+opcion.set("   Transformar a YIQ")  # Valor inicial
 
 # Crear botones 
-boton_abrir = tk.Button(ventana, text="Abrir", command= abrir_imagen, bg="#1dd767", font=("Roboto", 11))
+boton_abrir = tk.Button(ventana, text="Abrir ", command= abrir_imagen, bg="#1dd767", font=("Roboto", 11))
 boton_abrir.place(x=200, y=500)
 
-boton_transformar = tk.Button(ventana, text="Transformar a YIQ", command=transformar_a_yiq, bg="#1dd767",font=("Roboto", 11))
-boton_transformar.place(x=300, y=500)
+# menú desplegable (OptionMenu)
+menu_opciones = tk.OptionMenu(ventana, opcion, *operaciones.keys())
+menu_opciones.place(x=300, y=500)
 
 boton_guardar = tk.Button(ventana, text="Guardar", command=guardar_imagen, bg="#1dd767",font=("Roboto", 11))
 boton_guardar.place(x=500, y=500)
@@ -146,11 +172,9 @@ boton_guardar.place(x=500, y=500)
 boton_volver = tk.Button(ventana, text="Volver", command=volver, bg="#1dd767",font=("Roboto", 11))
 boton_volver.place(x=200, y=550)
 
-boton_transformar = tk.Button(ventana, text="Transformar a RGB", command=transformar_a_rgb, bg="#1dd767",font=("Roboto", 11))
-boton_transformar.place(x=300, y=550)
-
-boton_luminancia = tk.Button(ventana, text="Luminancia", command=luminancia, bg="#1dd767",font=("Roboto", 11))
-boton_luminancia.place(x=500, y=550)
+# Crear el botón y vincularlo a la función de solicitud de datos
+boton_ejecutar = tk.Button(ventana, text="Ejecutar Operación", command=ejecutar_opcion, bg="#1dd767", font=("Roboto", 11))
+boton_ejecutar.place(x=300, y=550)
 
 # Iniciar el loop de la ventana
 ventana.mainloop()
